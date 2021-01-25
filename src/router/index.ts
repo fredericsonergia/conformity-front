50fdd11099d68901578af3eb81ed108949778797
Matthieu Rossignon <matthieu.rossignon@student-cs.fr>
2021-01-25 10:33:27 +0100

    added presence

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
 import Surface from "../views/Surface.vue";
+import Presence from "../views/Presence.vue";
 
 Vue.use(VueRouter);
 
@@ -9,19 +10,24 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
     path: "/surface",
     name: "Surface",
-    component: Surface
-  }
+    component: Surface,
+  },
+  {
+    path: "/presence",
+    name: "Presence",
+    component: Presence,
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
